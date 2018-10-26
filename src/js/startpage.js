250bec95ec3c64403bd28d5a7b7f1b1928130915
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-10-26 19:36:12 -0400

    Added function for setting the background

--- src/js/startpage.js
+++ src/js/startpage.js
@@ -2,6 +2,11 @@
 
 var mustache = require("mustache");
 
+function set_background(url) {
+    var background = document.getElementById("background");
+    background.style.backgroundImage = `url(${url})`
+}
+
 function test() {
     var cards = document.getElementById("card-columns");
     var template = document.getElementById("template-card").innerHTML;
@@ -10,6 +15,8 @@ function test() {
     div.innerHTML = rendered.trim();
     cards.appendChild(div.firstChild);
 }
+
+set_background("https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&s=f11dd5509e0f6b4e253454f8583aff6c")
 
 test();
 test();
