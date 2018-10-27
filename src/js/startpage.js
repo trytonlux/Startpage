a8397d32d3ea1afc8aef8ac216c676b68c738288
Tryton Van Meer <trytonvanmeer@gmail.com>
2018-10-27 02:00:24 -0400

    Add cards from config

--- src/js/startpage.js
+++ src/js/startpage.js
@@ -2,25 +2,26 @@
 
 var mustache = require("mustache");
 
+// Globals
+var CARDS_CONTAINER = document.getElementById("card-columns");
+var CARD_TEMPLATE = document.getElementById("template-card").innerHTML;
+
+mustache.parse(CARD_TEMPLATE);
+
 function set_background(url) {
     var background = document.getElementById("background");
-    background.style.backgroundImage = `url(${url})`
+    background.style.backgroundImage = `url(${url})`;
 }
 
-function test() {
-    var cards = document.getElementById("card-columns");
-    var template = document.getElementById("template-card").innerHTML;
-    var rendered = mustache.render(template);
-    var div = document.createElement('div');
+function add_card(card) {
+    var rendered = mustache.render(CARD_TEMPLATE, card)
+    var div = document.createElement("div");
     div.innerHTML = rendered.trim();
-    cards.appendChild(div.firstChild);
+    CARDS_CONTAINER.appendChild(div.firstChild);
 }
 
-set_background(config["background"])
+set_background(config["background"]);
 
-test();
-test();
-test();
-test();
-test();
-test();
+config.cards.forEach(card => {
+    add_card(card);
+});
