1f6159ec40dcc43ef37fd31aee68145ca24f7276
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-03-15 00:34:48 -0400

    Assigned div holding links an ID instead of Class

--- src/script.js
+++ src/script.js
@@ -2,13 +2,10 @@
 
 // Setup markup for links from config.json and set body innerHTML.
 const markup = `
-<div class="links">
+<div id="links">
     ${links.map(link => `
         <a class="link" href="${link.href}">
-        <div class="link-inner">
-            <span class="mdi mdi-${link.icon}"></span>
             ${link.name}
-        </div>
         </a>
         `).join("")}
 </div>
