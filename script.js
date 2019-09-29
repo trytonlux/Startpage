5892038ad4eb11010a5bc495b086e76e6b8e8ace
Tryton Van Meer <trytonvanmeer@gmail.com>
2019-09-29 19:56:28 -0400

    Put li elements in ul.

--- script.js
+++ script.js
@@ -3,13 +3,15 @@
 // Setup markup for links from config.js and set body innerHTML.
 const markup = `
 <div id="links">
-    ${links.map(link => `
-        <li class="link">
-            <a href="${link.href}">
-                ${link.name}
-            </a>
-        </li>
-        `).join("")}
+    <ul>
+        ${links.map(link => `
+            <li class="link">
+                <a href="${link.href}">
+                    ${link.name}
+                </a>
+            </li>
+            `).join("")}
+    </ul>
 </div>
 `;
 
