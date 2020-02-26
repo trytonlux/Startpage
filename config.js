59836d99852eae814b0998a2fe73f6fe7909b4d9
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-02-25 20:01:39 -0500

    Don't set icons from config.

--- config.js
+++ config.js
@@ -1,34 +1,34 @@
 links = [
     // Developer
-    {name: "Github", href: "https://github.com", size: "802.15 KB"},
-    {name: "GitLab", href: "https://gitlab.com", size: "3.74 MB"},
-    {name: "GNOME GitLab", href: "https://gitlab.gnome.org", size: "1.05 MB"},
-    {name: "Arch Linux", href: "https://www.archlinux.org/", size: "60.93 KB"},
+    {name: "Github", href: "https://github.com"},
+    {name: "GitLab", href: "https://gitlab.com"},
+    {name: "GNOME GitLab", href: "https://gitlab.gnome.org"},
+    {name: "Arch Linux", href: "https://www.archlinux.org/"},
 
     // Social
-    {name: "Hangouts", href: "https://hangouts.google.com", size: "4.40 MB "},
-    {name: "Email", href: "https://mail.protonmail.com/inbox", size: "1.06 MB"},
-    {name: "Mastodon", href: "https://linuxrocks.online", size: "4.89 MB"},
-    {name: "YouTube", href: "https://youtube.com/feed/subscriptions", size: "4.22 MB"},
+    {name: "Hangouts", href: "https://hangouts.google.com"},
+    {name: "Email", href: "https://mail.protonmail.com/inbox"},
+    {name: "Mastodon", href: "https://linuxrocks.online"},
+    {name: "Invidious", href: "https://www.invidio.us/feed/subscriptions"},
 
     // Forums
-    {name: "Level1Techs Forums", href: "https://forum.level1techs.com", size: "4.51 MB"},
-    {name: "HE Forum", href:"https://forum.heavyelement.io", size: "1.11 MB"},
-    {name: "Destination Linux", href:"https://discourse.destinationlinux.network/", size: "4.38 MB"},
-    {name: "GNOME Discourse", href: "https://discourse.gnome.org/", size: "10.26 MB"},
-    {name: "LTT Forums", href: "https://linustechtips.com/main/", size: "2.61 MB"},
+    {name: "Level1Techs Forums", href: "https://forum.level1techs.com"},
+    {name: "HE Forum", href:"https://forum.heavyelement.io"},
+    {name: "Destination Linux", href:"https://discourse.destinationlinux.network/"},
+    {name: "GNOME Discourse", href: "https://discourse.gnome.org/"},
+    {name: "LTT Forums", href: "https://linustechtips.com/main/"},
 
     // Reddit
-    {name: "Reddit", href: "https://www.reddit.com", size: "980 KB"},
-    {name: "/m/Nintendo", href: "https://www.reddit.com/me/m/nintendo", size: "1.05 MB"},
-    {name: "/m/Linux", href: "https://www.reddit.com/me/m/linux", size: "1.03 MB"},
-    {name: "/r/Linux_Gaming", href: "https://www.reddit.com/r/linux_gaming", size: "1 MB"},
+    {name: "Reddit", href: "https://www.reddit.com"},
+    {name: "/m/Nintendo", href: "https://www.reddit.com/me/m/nintendo"},
+    {name: "/m/Linux", href: "https://www.reddit.com/me/m/linux"},
+    {name: "/r/Linux_Gaming", href: "https://www.reddit.com/r/linux_gaming"},
 
     // Gaming
-    {name: "Steam", href: "https://store.steampowered.com", size: "12.78 MB"},
-    {name: "GoG", href: "https://www.gog.com/", size: "3.35 MB"},
-    {name: "Humble Bundle", href: "https://www.humblebundle.com", size: "2.60 MB"},
+    {name: "Steam", href: "https://store.steampowered.com"},
+    {name: "GoG", href: "https://www.gog.com/"},
+    {name: "Humble Bundle", href: "https://www.humblebundle.com"},
 
-    {name: "Titanium Server", href: "http://titanium-server.lan", size: "720.65 KB"},
-    {name: "Wallhaven", href: "https://alpha.wallhaven.cc", size: "1.33 MB"}
+    // Other
+    {name: "Titanium Server", href: "http://titanium-server.lan"},
 ]
