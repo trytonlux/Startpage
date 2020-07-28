282c8c5acb0c306fdb744eb18f1f4836f3f08eb0
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-07-28 16:22:37 -0400

    Add initial prompt.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import links from "./config";
+import "./App.scss";
 
 class App extends React.Component
 {
     render()
     {
         return(
-            <h1>Hello, World!</h1>
+            <>
+            <main>
+                <span className="text-pink">~/Startpage</span>
+                <br/>
+                <span className="text-cyan">𝝺</span><span> ls</span>
+                <br/>
+            </main>
+            </>
         );
     }
 }
