c9e7858985e9846ae4166187857181a3a2aa382a
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-07-30 15:24:30 -0400

    Added function to config module to return element based on config.

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const links = [
+const config = [
     // Developer
     {name: "Github", href: "https://github.com"},
     {name: "GitLab", href: "https://gitlab.com"},
@@ -33,4 +33,18 @@ const links = [
     {name: "Titanium Server", href: "http://titanium-server.lan"},
 ]
 
-export default links;
+function config_to_element()
+{
+    // Return element consiting of <li> elements based on config.
+
+    let items = [];
+
+    for (let link of config)
+    {
+        items.push(`<li><a href="${link.href}">${link.name}<a/></li>`);
+    }
+
+    return items.join("");
+}
+
+export default config_to_element;
