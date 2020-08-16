76bd9a02f5e741240dd3dffbb1f5a28929cff6c6
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-08-15 21:05:43 -0400

    Add channels subpage.

---
+++ channels/index.js
@@ -0,0 +1,22 @@
+const links = [
+
+]
+
+function insert_links()
+{
+    let items = [];
+
+    for (let link of links)
+    {
+        items.push(
+            `<li><a href="${link.href}"><span class="yellow">"${link.name}"</span>,<a/></li>`
+        );
+    }
+
+    let ul = document.getElementById("link-list");
+    ul.innerHTML = items.join("");
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    insert_links();
+});
