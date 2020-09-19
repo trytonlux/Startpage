4d455063f57383e50b725b3f75ee89b5a1a40386
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-09-19 09:26:01 -0400

    Rename Channels with lowercase c.

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const links = [
     {name: "PCPartPicker", href: "https://ca.pcpartpicker.com/"},
     {name: "Linux Kernel Newbies", href: "https://kernelnewbies.org/LinuxChanges"},
     {name: "SteamGridDB", href: "https://www.steamgriddb.com"},
-    {name: "Channels", href: "./Channels"},
+    {name: "Channels", href: "./channels"},
 ]
 
 function insert_links()
