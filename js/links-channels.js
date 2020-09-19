4d98f5bedda28b30b3ab6f932f766f05e0afa13b
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-09-19 09:33:52 -0400

    Point to JS files in js/. Source links before index.js.

--- js/links-channels.js
+++ js/links-channels.js
@@ -89,22 +89,3 @@ const links = [
 
 // Sort channels by name alphabetically
 links.sort((a, b) => a.name.localeCompare(b.name));
-
-function insert_links()
-{
-    let items = [];
-
-    for (let link of links)
-    {
-        items.push(
-            `<li><a href="${link.href}"><span class="yellow">"${link.name}"</span>,<a/></li>`
-        );
-    }
-
-    let ul = document.getElementById("link-list");
-    ul.innerHTML = items.join("");
-}
-
-document.addEventListener("DOMContentLoaded", ()=> {
-    insert_links();
-});
