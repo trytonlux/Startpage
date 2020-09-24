d8ba94ec49837fd5f43ce25141ce10761dd52355
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-09-24 17:22:27 -0400

    Remove 3kliksphilip.

--- js/links-channels.js
+++ js/links-channels.js
@@ -1,5 +1,4 @@
 const links = [
-    {name: "3kliksphilip", href: "https://www.youtube.com/channel/UCmu9PVIZBk-ZCi-Sk2F2utA/videos"},
     {name: "AntVenom", href: "https://www.youtube.com/channel/UCK376qNDlNZZDNHsnaWuTeg/videos"},
     {name: "Arlo", href: "https://www.youtube.com/channel/UC-9QiiVaViuqMhLp1XEwdxA/videos"},
     {name: "Armoured Skeptic", href: "https://www.youtube.com/channel/UC1BWMtZbNLVMSFgwSukjqCw/videos"},
