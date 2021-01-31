60f1e17ce969183d2bd2a8496b17fdfb92bdc94f
Tryton Van Meer <trytonvanmeer@protonmail.com>
2021-01-31 14:11:52 -0500

    Add Overview component.

---
+++ src/Overview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import AppIcon from "./AppIcon";
+
+class Overview extends React.Component
+{
+    render()
+    {
+        return(
+            <div className="overview">
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+                <AppIcon icon="github" bg="#161b22" />
+            </div>
+        );
+    }
+}
+
+export default Overview;
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import "./main.scss";
 
 import React from "react";
 import ReactDOM from "react-dom";
+import Overview from "./Overview";
 import Dock from "./Dock";
 
 ReactDOM.render(
     <React.StrictMode>
+        <Overview />
         <Dock />
     </React.StrictMode>,
     document.getElementById("root")
