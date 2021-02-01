95b6a7fc737d714597bc6032561f500315b7264b
Tryton Van Meer <trytonvanmeer@protonmail.com>
2021-02-01 11:27:37 -0500

    Create ref to overview and pass function to Dock for toggling it.

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -4,12 +4,20 @@ import Dock from "./Dock";
 
 class Desktop extends React.Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.state = { show_overview: false };
+
+        this.overview = React.createRef();
+    }
+
     render()
     {
         return(
             <>
-            <Overview />
-            <Dock />
+            <Overview ref={this.overview} />
+            <Dock toggle_overview={() => this.overview.current.toggle_show()} />
             </>
         );
     }
