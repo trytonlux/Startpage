37173270eded77fe1988a579c837066eb7426c8d
Tryton Van Meer <trytonvanmeer@protonmail.com>
2021-02-04 16:58:39 -0500

    Read config and build AppIcon components.

--- src/Dock.tsx
+++ src/Dock.tsx
@@ -1,3 +1,5 @@
+import config from "./config.json";
+
 import React from "react";
 import AppIcon from "./AppIcon";
 
@@ -6,14 +8,44 @@ interface DockProps
     onOverviewLauncherClicked: () => void
 }
 
-class Dock extends React.Component<DockProps, {}>
+interface DockState
 {
+    apps: JSX.Element[]
+}
+
+class Dock extends React.Component<DockProps, DockState>
+{
+    state: DockState = {
+        apps: []
+    }
+
+    componentDidMount()
+    {
+        const dock_apps = config.Dock;
+        const apps: JSX.Element[] = [];
+
+        config.Apps.forEach((app) => {
+            if (dock_apps.includes(app.label))
+            {
+                apps.push(
+                    <AppIcon key={app.label} icon={app.icon} link={app.url} bg={app.color} />
+                );
+            }
+        });
+
+        this.setState({
+            apps: apps
+        });
+    }
+
     render()
     {
         return(
-        <div className="dock">
-            <AppIcon icon="launcher" onClick={this.props.onOverviewLauncherClicked} />
-        </div>
+            <div className="dock">
+                {this.state.apps}
+
+                <AppIcon icon="launcher" onClick={this.props.onOverviewLauncherClicked} />
+            </div>
         );
     }
 }
