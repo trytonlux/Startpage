ad592bf9d7f34b0b8b977a8145563329e7b31c3e
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-03-31 18:15:27 -0400

    embed index template and it's CSS file. serve them.

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	_ "embed"
+	"fmt"
+	"net/http"
+)
+
+//go:embed "data/index.html.tmpl"
+var indexHTMLTemplate string
+
+//go:embed "data/index.css"
+var indexCSS string
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, indexHTMLTemplate)
+}
+
+func resourceCSS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	fmt.Fprint(w, indexCSS)
+}
 
 func main() {
-	fmt.Println("Hello, World")
+	http.HandleFunc("/", index)
+	http.HandleFunc("/index.css", resourceCSS)
+
+	http.ListenAndServe(":8080", nil)
 }
