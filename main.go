2cf05ae1dc14d18045efd064a84fea86fccd1c38
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-04-08 19:36:09 -0400

    add render method to abstract getting the template and executing it.

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"embed"
-	_ "embed"
+	"fmt"
 	"net/http"
 	"text/template"
 )
@@ -15,6 +15,18 @@ type Link struct {
 //go:embed "assets/*"
 var assets embed.FS
 
+func render(w http.ResponseWriter, name string, data any) (err error) {
+	template_name := fmt.Sprintf("assets/%s.html.tmpl", name)
+
+	template, err := template.ParseFS(assets, template_name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return template.Execute(w, data)
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	test := []Link{
 		{"GitHub", "github.com"},
@@ -22,13 +34,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 		{"Steam", "store.steampowered.com"},
 	}
 
-	template, err := template.ParseFS(assets, "assets/index.html.tmpl")
-
-	if err != nil {
-		panic(err)
-	}
-
-	err = template.Execute(w, test)
+	err := render(w, "index", test)
 
 	if err != nil {
 		panic(err)
