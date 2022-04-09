e2518fdc3212cebbc03bb02f875f22a4c9167f78
Tryton Van Meer <hello@trytonvanmeer.dev>
2022-04-09 13:06:56 -0400

    use sass for stylesheet.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module startpage
 
 go 1.18
+
+require github.com/bep/golibsass v1.0.0 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+
+	"github.com/bep/golibsass/libsass"
 )
 
 type Link struct {
@@ -14,6 +16,32 @@ type Link struct {
 
 //go:embed "assets/*"
 var assets embed.FS
+var css []byte = generateCSS()
+
+func generateCSS() []byte {
+	source, err := assets.ReadFile("assets/index.scss")
+
+	if err != nil {
+		panic(err)
+	}
+
+	transpiler, _ := libsass.New(libsass.Options{
+		OutputStyle: libsass.CompressedStyle,
+	})
+
+	result, err := transpiler.Execute(string(source))
+
+	if err != nil {
+		panic(err)
+	}
+
+	return []byte(result.CSS)
+}
+
+func serveCSS(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/css")
+	w.Write(css)
+}
 
 func render(w http.ResponseWriter, name string, data any) {
 	template_name := fmt.Sprintf("assets/%s.html.tmpl", name)
@@ -26,12 +54,6 @@ func render(w http.ResponseWriter, name string, data any) {
 	}
 
 	template.Execute(w, data)
-}
-
-func serveCSS(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/css")
-	css, _ := assets.ReadFile("assets/index.css")
-	w.Write(css)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
