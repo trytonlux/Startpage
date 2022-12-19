f814f30f59962df9124570310b869ccad4b78b22
Tryton Van Meer <code@trytonvanmeer.dev>
2022-12-19 00:34:07 -0500

    create Link component.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use sycamore::prelude::*;
 
+#[component(inline_props)]
+fn Link<G: Html>(cx: Scope, name: String, url: String) -> View<G> {
+    view! {cx,
+        div(class="link") {
+            a(href=(url)) {(name)}
+        }
+    }
+}
+
 fn main() {
     sycamore::render(|cx| {
         view! { cx,
-            p { "Hello, World!" }
+            Link(name="Steam".to_string(), url="https://store.steampowered.com".to_string())
         }
     });
 }
