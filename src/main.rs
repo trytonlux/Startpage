b72a6c1f4da22867441efd86508f79e82872f804
Tryton Van Meer <code@trytonvanmeer.dev>
2022-12-21 20:28:55 -0500

    select a colour at random for Link background.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
+use rand::prelude::*;
 use sycamore::prelude::*;
+
+fn random_color() -> String {
+    vec![
+        "flamingo", "pink", "mauve", "red", "maroon", "peach", "yellow", "green", "teal", "sky",
+        "sapphire", "blue", "lavender",
+    ]
+    .choose_multiple(&mut rand::thread_rng(), 1)
+    .cloned()
+    .collect()
+}
 
 #[component(inline_props)]
 fn Link<G: Html>(cx: Scope, name: String, url: String) -> View<G> {
     view! {cx,
-        a(class="link", href=(url)) {(name)}
+        a(class=(format!("link {}", random_color())), href=(url)) {(name)}
     }
 }
 
