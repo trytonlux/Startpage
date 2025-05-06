2c4474e1184b5473fd10b4a154f009df7fe2c09a
Tryton Lux <code@trytonlux.ca>
2025-05-06 19:32:46 -0400

    feat: add logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use actix_files::Files;
-use actix_web::{App, HttpResponse, HttpServer, Responder, get, web::Data};
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, web::Data};
+use env_logger::Env;
 use tera::{Context, Tera};
 
 mod widgets;
@@ -15,10 +16,13 @@ async fn index(tera: Data<Tera>) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     let tera = Data::new(Tera::new("templates/**/*").unwrap());
 
     HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .app_data(tera.clone())
             .service(index)
             .service(Files::new("/assets", "./assets").show_files_listing())
